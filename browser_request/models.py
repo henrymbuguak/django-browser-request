6c83ab349c9d2beb26f55a73847e424a4a465f0a
Henry Mbugua <hmbugua@resolution.co.ke>
2020-02-19 18:11:34 +0300

    Creation of the model

--- browser_request/models.py
+++ browser_request/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class UserBrowserData(BaseModel):
+    ip_address = models.CharField(max_length=200)
+    user_browser = models.CharField(max_length=200)
+    user_content_type = models.CharField(max_length=200)
+    user_query_string = models.CharField(max_length=200)
+
+
